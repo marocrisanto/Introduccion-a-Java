f67b17467f3d5023952aca61e8727aa308de5b73
marocrisanto <crisantojuaomar@gmail.com>
2023-05-19 23:45:56 -0600

    añade archivos

---
+++ src/incial/Incial.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package incial;
+
+/**
+ *
+ * @author maroc
+ */
+public class Incial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String nombre = "JUAN";
+    boolean bandera =false;
+        char i;
+}
+}
+    
---
+++ src/incial/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package incial;
+/**
+ *
+ * @author maroc
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {    
+    int numero = 48;
+    double decimales = 3.55;
+    boolean bandera = false;
+    }
+}
+    
